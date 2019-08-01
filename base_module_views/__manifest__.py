c4f180a726e249b9adfe8d2659256682035cf6d6
Andrius Laukavičius <andrius@focusate.eu>
2019-08-01 09:16:25 +0300

    [ADD] base_module_views: init
    * Removes default Apps filter, so it now shows all modules by default.
    * Makes tree view default view, when opening views.
    
    NOTE. This module will be auto installed, if you will add it in your
    addons path.
    
    [BRANCH] feature/no-default-module-filter

---
+++ base_module_views/__manifest__.py
@@ -0,0 +1,18 @@
+# Author: Andrius Laukavičius. Copyright: Andrius Laukavičius.
+# See LICENSE file for full copyright and licensing details.
+{
+    'name': "Apps View Changes",
+    'version': '1.0.0',
+    'summary': 'apps, modules, view changes',
+    'license': 'LGPL-3',
+    'author': "Andrius Laukavičius",
+    'website': "https://timefordev.com",
+    'category': 'Hidden',
+    'depends': [
+        'base'
+    ],
+    'data': [
+        'views/ir_module_views.xml',
+    ],
+    'auto_install': True,
+}
