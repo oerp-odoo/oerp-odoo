edcecd87711b1828abc5727f8c602589c46789a3
Andrius Laukavičius <andrius@focusate.eu>
2019-08-06 17:31:11 +0300

    [FIX] base_module_views: Hidden -> Extra Tools
    Apparently module must have category that is used on apps, in order to
    be visible there, so using one that is used on Apps.

--- base_module_views/__manifest__.py
+++ base_module_views/__manifest__.py
@@ -2,12 +2,12 @@
 # See LICENSE file for full copyright and licensing details.
 {
     'name': "Apps View Changes",
-    'version': '1.0.0',
+    'version': '1.0.1',
     'summary': 'apps, modules, view changes',
     'license': 'LGPL-3',
     'author': "Andrius Laukavičius",
     'website': "https://timefordev.com",
-    'category': 'Hidden',
+    'category': 'Extra Tools',
     'depends': [
         'base'
     ],
