1abea7fc4d6c98e1c32f57d4e47f1f85924745d0
Andrius Laukavičius <anlaukavic@gmail.com>
2022-10-28 18:31:19 +0300

    [FIX] product_main_on_order: demo data
    Move demo data to `demo` section as it was accidentally put on `data`
    section.

--- product_main_on_order/__manifest__.py
+++ product_main_on_order/__manifest__.py
@@ -2,7 +2,7 @@
 # See LICENSE file for full copyright and licensing details.
 {
     'name': "Main Product on Order",
-    'version': '15.0.1.0.0',
+    'version': '15.0.1.0.1',
     'summary': 'Rules to find main product on order',
     'license': 'LGPL-3',
     'author': "Andrius Laukavičius",
@@ -15,6 +15,8 @@
     'data': [
         'security/ir.model.access.csv',
         'views/product_main_rule_views.xml',
+    ],
+    'demo': [
         'demo/product_main_rule.xml',
     ],
     'installable': True,
