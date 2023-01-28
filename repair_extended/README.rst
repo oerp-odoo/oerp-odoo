045aa2583352ec6d5d9ea26e9c399df20acc0ea3
Andrius Laukavičius <anlaukavic@gmail.com>
2023-01-28 21:25:26 +0200

    [ADD] repair_extended: init
    Adds some quality of life improvements for ``repair`` module:
    
    * ``Tags`` are visible in list view.

---
+++ repair_extended/README.rst
@@ -0,0 +1,11 @@
+Repair - Extended
+#################
+
+Adds some quality of life improvements for ``repair`` module:
+
+* ``Tags`` are visible in list view.
+
+Contributors
+============
+
+* Andrius Laukavičius (timefordev)
--- repair_extended/__manifest__.py
+++ repair_extended/__manifest__.py
@@ -1,21 +1,18 @@
 # Author: Andrius Laukavičius. Copyright: Andrius Laukavičius.
 # See LICENSE file for full copyright and licensing details.
 {
-    'name': "Repairs Category",
-    'version': '15.0.2.0.0',
-    'summary': 'Use categories to distinguish different type of repairs',
+    'name': "Repair - Extended",
+    'version': '15.0.1.0.0',
+    'summary': 'Extra features for repair module',
     'license': 'LGPL-3',
     'author': "Andrius Laukavičius",
     'website': "https://timefordev.com",
-    'category': 'Manufacturing/Maintenance',
+    'category': 'Inventory/Inventory',
     'depends': [
         # odoo
         'repair',
     ],
     'data': [
-        'security/ir.model.access.csv',
-        'security/repair_category_security.xml',
-        'views/repair_category_views.xml',
         'views/repair_order_views.xml',
     ],
     'installable': True,
