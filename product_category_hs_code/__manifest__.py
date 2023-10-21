d98ca07a4b5dc2f8804884521083e4f10cf1ad4b
Andrius Laukavičius <anlaukavic@gmail.com>
2023-10-21 16:17:54 +0300

    [ADD] product_category_hs_code: init
    Adds HS Code field on product category. When creating product, it will
    use its category ``HS Code`` value if not specified on product.

---
+++ product_category_hs_code/__manifest__.py
@@ -0,0 +1,18 @@
+# Author: Andrius Laukavičius. Copyright: Andrius Laukavičius.
+# See LICENSE file for full copyright and licensing details.
+{
+    'name': "Product Category - HS Code",
+    'version': '15.0.1.0.0',
+    'summary': 'HS Code on product category',
+    'license': 'LGPL-3',
+    'author': "Andrius Laukavičius",
+    'website': "https://timefordev.com",
+    'category': 'Inventory/Delivery',
+    'depends': [
+        # odoo
+        'delivery',
+    ],
+    'data': ['views/product_category.xml'],
+    'installable': True,
+    'auto_install': True,
+}
