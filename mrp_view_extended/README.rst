6d5a814900278dadda76453630be69cd85ce7043
Andrius Laukavičius <anlaukavic@gmail.com>
2023-11-25 09:10:09 +0200

    [ADD] mrp_view_extended: init
    Quality of life changes for MRP views:
    
    * ``date_finished`` field is now visible in manufacturing orders list
      view.
    * ``date_finished`` field is now visible in manufacturing order form
      view (when set).

---
+++ mrp_view_extended/README.rst
@@ -0,0 +1,12 @@
+MRP - View Changes
+##################
+
+Quality of life changes for MRP views:
+
+* ``date_finished`` field is now visible in manufacturing orders list view.
+* ``date_finished`` field is now visible in manufacturing order form view (when set).
+
+Contributors
+============
+
+* Andrius Laukavičius (timefordev)
---
+++ mrp_view_extended/__manifest__.py
@@ -0,0 +1,19 @@
+# Author: Andrius Laukavičius. Copyright: Andrius Laukavičius.
+# See LICENSE file for full copyright and licensing details.
+{
+    'name': "MRP - View Changes",
+    'version': '15.0.1.0.0',
+    'summary': 'Quality of life MRP view changes',
+    'license': 'LGPL-3',
+    'author': "Andrius Laukavičius",
+    'website': "https://timefordev.com",
+    'category': 'Manufacturing/Manufacturing',
+    'depends': [
+        # odoo
+        'mrp',
+    ],
+    'data': [
+        'views/mrp_production_views.xml',
+    ],
+    'installable': True,
+}
