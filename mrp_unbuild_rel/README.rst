eac47a4dc152e18be3bdf138df2bccd13851280a
Andrius Laukavičius <anlaukavic@gmail.com>
2023-11-25 14:24:17 +0200

    [ADD] mrp_unbuild_rel: init

---
+++ mrp_unbuild_rel/README.rst
@@ -0,0 +1,13 @@
+MRP - Unbuild Relation
+######################
+
+Adds one2many relation on manufacturing orders, to show related unbuild
+orders.
+
+Manufacturing orders can be filtered whether that order is unbuilt. Also
+we can open unbuild orders from related manufacturing order.
+
+Contributors
+============
+
+* Andrius Laukavičius (timefordev)
---
+++ mrp_unbuild_rel/models/mrp_production.py
@@ -0,0 +1,26 @@
+from odoo import models, fields, api
+
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    unbuild_ids = fields.One2many(
+        'mrp.unbuild',
+        'mo_id',
+        string="Unbuild Orders",
+        auto_join=True,
+    )
+    unbuild_count = fields.Integer(compute='_compute_unbuild_stats')
+    is_unbuilt = fields.Boolean(compute='_compute_unbuild_stats')
+
+    @api.depends('unbuild_ids')
+    def _compute_unbuild_stats(self):
+        for mo in self:
+            count = len(mo.unbuild_ids)
+            mo.update({'unbuild_count': count, 'is_unbuilt': bool(count)})
+
+    def action_view_unbuild_orders(self):
+        self.ensure_one()
+        action = self.env['ir.actions.actions']._for_xml_id('mrp.mrp_unbuild')
+        action['domain'] = [('mo_id', '=', self.id)]
+        return action
