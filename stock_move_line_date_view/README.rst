8add171326dc427f9d33a77b74a68db1dbd1f465
Andrius Laukavičius <anlaukavic@gmail.com>
2024-01-27 09:35:21 +0200

    [ADD] stock_move_line_date_view: init
    Show Date field in stock move line view via picking

---
+++ stock_move_line_date_view/README.rst
@@ -0,0 +1,9 @@
+Stock Move Line Date in View
+###############################
+
+Show Date field in stock move line view via picking
+
+Contributors
+============
+
+* Author: Andrius Laukavičius (timefordev)
--- stock_move_line_date_view/__manifest__.py
+++ stock_move_line_date_view/__manifest__.py
@@ -1,9 +1,9 @@
 # Author: Andrius Laukavičius. Copyright: Andrius Laukavičius.
 # See LICENSE file for full copyright and licensing details.
 {
-    'name': "Stock - No quick create on form",
+    'name': "Stock Move Line Date in View",
     'version': '15.0.1.0.0',
-    'summary': 'Will not show quick create option on some fields',
+    'summary': 'Show Date field in stock move line view via picking',
     'license': 'LGPL-3',
     'author': "Andrius Laukavičius",
     'website': "https://timefordev.com",
