33864738f6dc37cf9763094519676f7b0dc532ee
Andrius Laukavičius <anlaukavic@gmail.com>
2026-03-07 10:47:31 +0200

    [IMP] auth_basic: hash credentials
    Its better to hash credentials, so even if we show it in logs or its
    exposed via cash, credentials will be obfuscated and it will not be
    possible to retrieve real ones.

--- auth_basic/models/auth_basic.py
+++ auth_basic/models/auth_basic.py
@@ -5,6 +5,8 @@ from footil.xtyping import bytes_to_str, str_to_bytes
 from odoo import _, api, fields, models, tools
 from odoo.exceptions import AccessError, ValidationError
 
+from ..utils import make_hash
+
 CACHE_DEPS = ['username', 'password']
 
 
@@ -21,7 +23,7 @@ class AuthBasic(models.Model):
     user_id = fields.Many2one(
         'res.users',
         required=True,
-        help="The user used to process the requests authenticated by " "basic auth",
+        help="The user used to process the requests authenticated by basic auth",
     )
 
     _sql_constraints = [
@@ -55,7 +57,7 @@ class AuthBasic(models.Model):
         """
         self.ensure_one()
         byte_creds = base64.b64encode(str_to_bytes(self._raw_credentials))
-        return bytes_to_str(byte_creds)
+        return make_hash(bytes_to_str(byte_creds))
 
     @api.model
     def _retrieve_auth_basic(self, credentials):
@@ -69,7 +71,7 @@ class AuthBasic(models.Model):
         for auth_basic in self.search([]):
             if tools.consteq(credentials, auth_basic._credentials):
                 return auth_basic.id
-        raise ValidationError(_("The credentials %s are incorrect") % credentials)
+        raise ValidationError(_("The credentials %s are incorrect") % credentials[:8])
 
     def _clear_key_cache(self):
         self._retrieve_auth_basic_id.clear_cache(self.env[self._name])
