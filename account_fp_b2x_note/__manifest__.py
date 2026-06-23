03870ad408c6a5ba6b727ee4ba35faa74434e420
Andrius Laukavičius <anlaukavic@gmail.com>
2026-06-23 11:04:50 +0300

    [19.0][MIG] account_fp_b2x_note

--- account_fp_b2x_note/__manifest__.py
+++ account_fp_b2x_note/__manifest__.py
@@ -2,7 +2,7 @@
 # See LICENSE file for full copyright and licensing details.
 {
     'name': "Fiscal Position B2C/B2B Notes",
-    'version': '15.0.2.0.0',
+    'version': '19.0.3.0.0',
     'summary': 'Notes per fiscal position for B2C/B2B purposes',
     'license': 'LGPL-3',
     'author': "Andrius Laukavičius",
@@ -15,5 +15,5 @@
     'data': [
         'views/account_fiscal_position_views.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
