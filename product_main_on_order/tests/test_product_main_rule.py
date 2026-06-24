9aa86905d0b23dbb8417f5ecd037f12825f19c10
Andrius Laukavičius <anlaukavic@gmail.com>
2026-06-24 09:45:39 +0300

    [19.0][MIG] product_main_on_order

--- product_main_on_order/tests/test_product_main_rule.py
+++ product_main_on_order/tests/test_product_main_rule.py
@@ -3,59 +3,92 @@ from odoo.tests import common
 
 
 class TestProductMainRule(common.TransactionCase):
-    """Test class for product main rule functionality."""
-
     @classmethod
     def setUpClass(cls):
-        """Set demo data for product main rule tests."""
         super().setUpClass()
+        cls.ProductProduct = cls.env['product.product']
         cls.ProductMainRule = cls.env['product.main.rule']
-        cls.product_main_rule_1 = cls.env.ref(
-            'product_main_on_order.product_main_rule_1'
+        (
+            cls.product_in_rule_1,
+            cls.product_in_rule_2,
+            cls.product_in_rule_3,
+            cls.product_not_in_rule_1,
+            cls.product_not_in_rule_2,
+            cls.product_not_in_rule_3,
+        ) = cls.ProductProduct.create(
+            [
+                {'name': 'MY-PRODUCT-1'},
+                {'name': 'MY-PRODUCT-2'},
+                {'name': 'MY-PRODUCT-3'},
+                {'name': 'MY-PRODUCT-4'},
+                {'name': 'MY-PRODUCT-5'},
+                {'name': 'MY-PRODUCT-6'},
+            ]
         )
-        cls.product_main_rule_2 = cls.env.ref(
-            'product_main_on_order.product_main_rule_2'
+        (
+            cls.product_main_rule_1,
+            cls.product_main_rule_2,
+            cls.product_main_rule_3,
+            cls.product_main_rule_4,
+        ) = cls.ProductMainRule.create(
+            [
+                {
+                    'name': 'MY-RULE-1',
+                    'product_id': cls.product_in_rule_1.id,
+                    'sequence': 1,
+                },
+                {
+                    'name': 'MY-RULE-2-FALLBACK',
+                    'product_id': cls.product_in_rule_2.id,
+                    'is_fallback': True,
+                    'sequence': 5,
+                },
+                {
+                    'name': 'MY-RULE-3',
+                    'product_id': cls.product_in_rule_3.id,
+                    'sequence': 10,
+                },
+                {
+                    'name': 'MY-RULE-4',
+                    'product_id': cls.product_in_rule_2.id,
+                    'sequence': 15,
+                },
+            ]
         )
-        cls.product_main_rule_3 = cls.env.ref(
-            'product_main_on_order.product_main_rule_3'
-        )
-        cls.product_main_rule_4 = cls.env.ref(
-            'product_main_on_order.product_main_rule_4'
-        )
-        # Ones that are in rules.
-        cls.product_5 = cls.env.ref('product.product_product_5')
-        cls.product_6 = cls.env.ref('product.product_product_6')
-        cls.product_7 = cls.env.ref('product.product_product_7')
-        # Ones not in rules.
-        cls.product_8 = cls.env.ref('product.product_product_8')
-        cls.product_9 = cls.env.ref('product.product_product_9')
-        cls.product_10 = cls.env.ref('product.product_product_10')
 
-    def test_01_get_main_rule(self):
-        """Find main rule when when product is matched."""
+    def test_01_get_main_rule_matched(self):
+        # WHEN
         res = self.ProductMainRule.get_main_rule(
-            self.product_8 | self.product_6 | self.product_10
+            self.product_not_in_rule_1
+            | self.product_in_rule_2
+            | self.product_not_in_rule_3
         )
+        # THEN
         self.assertEqual(res, self.product_main_rule_4)
 
-    def test_02_get_main_rule(self):
-        """Find main rule when fallback is used."""
+    def test_02_get_main_rule_fallback(self):
+        # WHEN
         res = self.ProductMainRule.get_main_rule(
-            self.product_8 | self.product_9 | self.product_10
+            self.product_not_in_rule_1
+            | self.product_not_in_rule_2
+            | self.product_not_in_rule_3
         )
-        # Fallback rule.
+        # THEN
         self.assertEqual(res, self.product_main_rule_2)
 
-    def test_03_get_main_rule(self):
-        """Try finding rule without any match and fallback rule."""
+    def test_03_get_main_rule_not_found(self):
+        # GIVEN
         self.product_main_rule_2.is_fallback = False
+        # WHEN
         res = self.ProductMainRule.get_main_rule(
-            self.product_8 | self.product_9 | self.product_10
+            self.product_not_in_rule_1
+            | self.product_not_in_rule_2
+            | self.product_not_in_rule_3
         )
+        # THEN
         # Fallback rule.
         self.assertEqual(res, self.ProductMainRule)
 
     def test_04_check_fallback_rule_unique(self):
-        """Check if only single fallback rule is allowed."""
         with self.assertRaises(ValidationError):
             self.product_main_rule_1.is_fallback = True
