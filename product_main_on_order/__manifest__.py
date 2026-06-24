b9e068d3a1bcb544d0e53c198c0d29841bdc80b8
Andrius Laukavičius <anlaukavic@gmail.com>
2026-06-24 09:45:39 +0300

    [19.0][MIG] product_main_on_order

--- product_main_on_order/__manifest__.py
+++ product_main_on_order/__manifest__.py
@@ -2,22 +2,19 @@
 # See LICENSE file for full copyright and licensing details.
 {
     'name': "Main Product on Order",
-    'version': '15.0.1.0.1',
+    'version': '19.0.2.0.0',
     'summary': 'Rules to find main product on order',
     'license': 'LGPL-3',
     'author': "Andrius Laukavičius",
     'website': "https://timefordev.com",
-    'category': 'Hidden/Tools',
+    'category': 'Sales/Sales',
     'depends': [
         # odoo
         'product',
     ],
     'data': [
         'security/ir.model.access.csv',
-        'views/product_main_rule_views.xml',
+        'views/product_main_rule.xml',
     ],
-    'demo': [
-        'demo/product_main_rule.xml',
-    ],
-    'installable': False,
+    'installable': True,
 }
