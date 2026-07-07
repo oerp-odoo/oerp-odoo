80f342d1ef2236977e61d00ca811640fc3797088
Andrius Laukavičius <anlaukavic@gmail.com>
2026-07-07 13:43:49 +0300

    [19.0][MIG] repair_category

--- repair_category/models/__init__.py
+++ repair_category/models/__init__.py
@@ -1,3 +1,1 @@
-from . import repair_category, repair_order, repair_line, repair_fee
-
-__all__ = [repair_category, repair_order, repair_line, repair_fee]
+from . import repair_category, repair_order
