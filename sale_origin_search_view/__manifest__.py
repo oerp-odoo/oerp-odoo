4b833b6a871719a793ccf5282bc68ea3350ab67b
Andrius Laukavičius <anlaukavic@gmail.com>
2026-07-13 14:52:24 +0300

    [19.0][MIG] sale_origin_search_view

--- sale_origin_search_view/__manifest__.py
+++ sale_origin_search_view/__manifest__.py
@@ -2,7 +2,7 @@
 # See LICENSE file for full copyright and licensing details.
 {
     'name': "Source Document in Sale Orders Search Bar",
-    'version': '15.0.1.0.0',
+    'version': '19.0.2.0.0',
     'summary': 'Source document in Sale Order search view',
     'license': 'LGPL-3',
     'author': "Andrius Laukavičius",
@@ -13,7 +13,7 @@
         'sale',
     ],
     'data': [
-        'views/sale_order_views.xml',
+        'views/sale_order.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
