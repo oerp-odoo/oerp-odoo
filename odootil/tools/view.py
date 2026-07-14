c5f0854844addcbcf69fa187db5a5fe284de321b
Andrius Laukavičius <anlaukavic@gmail.com>
2026-07-14 10:42:08 +0300

    [19.0][FIX] odootil: preprocess_arch_readonly_fields
    We must exclude all fields that are explicitly readonly via backend
    (python), because otherwise we would be overwriting strict readonly
    making it conditional.

--- odootil/tools/view.py
+++ odootil/tools/view.py
@@ -1,8 +1,10 @@
 from lxml import etree
 
+from odoo.models import BaseModel
+
 
 def preprocess_arch_readonly_fields(
-    arch, readonly_condition, ignored_fields=None, xpath='//field'
+    model: BaseModel, arch, readonly_condition, ignored_fields=None, xpath='//field'
 ):
     if ignored_fields is None:
         ignored_fields = []
@@ -11,7 +13,7 @@ def preprocess_arch_readonly_fields(
     if not field_nodes:
         return arch
     for field_node in field_nodes:
-        if field_node.get('name') in ignored_fields:
+        if _is_readonly_ignored_field(model, field_node, ignored_fields):
             continue
         orig_readonly = field_node.get('readonly')
         if orig_readonly:
@@ -20,3 +22,17 @@ def preprocess_arch_readonly_fields(
             readonly = readonly_condition
         field_node.set('readonly', readonly)
     return etree.tostring(archnode, encoding='unicode')
+
+
+def _is_readonly_ignored_field(model: BaseModel, field_node, ignored_fields: list[str]):
+    fname = field_node.get('name')
+    if fname in ignored_fields:
+        return True
+    # If view has readonly condition, it means python's readonly is
+    # already overwritten.
+    if field_node.get('readonly'):
+        return False
+    field = model._fields.get(fname)
+    # We ignore if its made readonly in python as we would be overwriting
+    # it with conditional readonly then.
+    return field.readonly
